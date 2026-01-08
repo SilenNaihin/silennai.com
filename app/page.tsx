1273b34b6f9cc17b80359ec49b19f20a17ecb923
Silen Naihin <silen.naihin@gmail.com>
2026-01-08 13:36:54 -0500

    updating focus

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,21 @@ export default function Home() {
             >
               Genesis Mission
             </a>{' '}
-            to accelerate scientific discovery with AI. Akin to the Manhattan
-            project.
+            to accelerate scientific discovery with AI. The{' '}
+            <a
+              href="https://www.whitehouse.gov/presidential-actions/2025/11/launching-the-genesis-mission/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline hover:text-gray-900"
+            >
+              modern day Manhattan Project
+            </a>
+            .
           </li>
           <li>
-            My time is focused on expanding the frontier of human knowledge.
+            I&apos;m focused on expanding humanity&apos;s epistemic frontier.
+            Exploring ideas in continual learning, OOD generation,
+            verifiability, and alternate architectures.
           </li>
           <li>
             I ran a company that was in YC (StackwiseAI) for two years. Before
@@ -52,7 +62,7 @@ export default function Home() {
               paper
             </a>{' '}
             on a portion of that work. I&apos;ve also done various ML and AI eng
-            jobs as a contractor/founding engineer. I dropped out of a cs degree
+            jobs as a contractor/founding engineer. I dropped out of a CS degree
             at Minerva University.
           </li>
         </ul>
@@ -92,7 +102,7 @@ export default function Home() {
           </li>
           <li>
             I&apos;m currently working on hitting the 1000lb club and a 3 plate
-            bench press. I&apos;m relatively close.
+            bench press.
           </li>
           <li>
             I&apos;ve visited 30+ countries across 6 continents. I&apos;ve lived
